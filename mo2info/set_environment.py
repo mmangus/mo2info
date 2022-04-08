84f387d8b6e6c06ca5ad9df74e326bf31f8a5c65
Michael Mangus <mmangus@gmail.com>
2022-04-08 13:08:09 -0700

    give the correct abs path

--- mo2info/set_environment.py
+++ mo2info/set_environment.py
@@ -3,7 +3,7 @@ import os
 
 
 def set_environment():
-    with open("/var/app/staging/env.json", "r") as envfile:
+    with open("/mo2info/env.json", "r") as envfile:
         eb_env = json.load(envfile)
         for k,v in eb_env.items():
             os.environ.setdefault(k, v)
