9bd927b0dbdd0249afd48d973a633066d8f2f501
Michael Mangus <mmangus@gmail.com>
2022-04-08 12:43:24 -0700

    banging rocks

--- mo2info/set_environment.py
+++ mo2info/set_environment.py
@@ -3,7 +3,7 @@ import os
 
 
 def set_environment():
-    with open("env.json", "r") as envfile:
+    with open("../env.json", "r") as envfile:
         eb_env = json.load(envfile)
         for k,v in eb_env.items():
             os.environ.setdefault(k, v)
