038dccfc1eb1217505ef32633f380b5a76cfff61
Michael Mangus <mmangus@gmail.com>
2022-04-08 21:26:04 -0700

    rework damage logging

--- mo2info/main/models.py
+++ mo2info/main/models.py
@@ -1,5 +1,9 @@
-from django.core.validators import MinValueValidator
+import re
+
+from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
+
+DAMAGE_LOG_RE = re.compile(r"((\d+)(?:\s*)){10}")
 
 class BowDamageTrial(models.Model):
     class BowTypeChoices(models.TextChoices):
@@ -20,6 +24,14 @@ class BowDamageTrial(models.Model):
         validators=[MinValueValidator(0.1)],
         help_text="Maximum durability of the bow (second number in tooltip)"
     )
+    damage_log = models.TextField(
+        validators = [
+            RegexValidator(
+                DAMAGE_LOG_RE,
+                message="Enter just the 10 numbers, 1 number per line"
+            )
+        ]
+    )
     mean_damage = models.FloatField(
         validators=[MinValueValidator(0.0)],
         help_text="The average damage per shot to the target dummy's head"
@@ -27,3 +39,10 @@ class BowDamageTrial(models.Model):
 
     def __str__(self) -> str:
         return f"{self.bow_type} @ {self.range}: {self.mean_damage}"
+
+
+    def save(self, *args, **kwargs) -> None:
+        self.mean_damage = sum(
+            map(int, self.damage_log.split())
+        ) / 10
+        return super().save(*args, **kwargs)
--- mo2info/main/views.py
+++ mo2info/main/views.py
@@ -1,14 +1,17 @@
+from django.urls import reverse
 from django.views.generic import CreateView
 
 from .models import BowDamageTrial
 
 class BowDamageTrialCreateView(CreateView):
     model = BowDamageTrial
-    success_url = "/mo2/bow-damage/contribute/"
     fields = [
         "bow_type",
         "range",
         "durability_current",
         "durability_max",
-        "mean_damage",
+        "damage_log",
     ]
+
+    def get_success_url(self) -> str:
+        return reverse('bow-damage-create')
